72749232ef2d156863e2e534df0e4fbae2078c03
Olakunle Arewa <arewa.olakunle@gmail.com>
2020-06-28 13:25:39 +0100

    feat: users need to be able to revoke a token without knowing it
    Sometimes, users would need to for instance a revoke a user's token
    just by knowing the user's ID(which they can use to generate a
    token). So I replaced random generation of tokens with the hmac of
    the key or passed key. All this relies on the fact that our secret
    key is never stolen 😭

--- auth/token.go
+++ auth/token.go
@@ -1,7 +1,11 @@
 package auth
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"time"
 
@@ -10,21 +14,57 @@ import (
 
 const tokenMapKey = "general_token_map"
 
+var (
+	ErrTokenNotFound = errors.New("The passed token has either expired or never existed")
+)
+
 type TokenStore struct {
-	redis *redis.Client
+	redis  *redis.Client
+	secret []byte
 }
 
-func NewTokenStore(r *redis.Client) *TokenStore {
-	return &TokenStore{redis: r}
+func NewTokenStore(r *redis.Client, secret []byte) *TokenStore {
+	return &TokenStore{redis: r, secret: secret}
 }
 
-// New creates a 32 character single use token that expires after the given timeout.
-func (ts *TokenStore) New(timeout time.Duration, data interface{}) (string, error) {
-	var token string
+// Commission creates a single use token that expires after the given timeout. The token is a hash of a
+// random 32 character key that can be used to revoke the token later on.
+func (ts *TokenStore) Commission(t time.Duration, data interface{}) (token string, key string, err error) {
+	var encoded []byte
+
+	if key, err = RandomString(32); err != nil {
+		return
+	}
+
+	// create a hash for the key
+	if token, err = ts.getHash(key); err != nil {
+		return
+	}
+
+	// TODO: replace this something lighter and faster
+	if encoded, err = json.Marshal(data); err != nil {
+		return
+	}
+
+	// One would naturally prefer hash maps but they don't support individual subkey expiry.
+	tokenKey := fmt.Sprintf("%s::%s", tokenMapKey, token)
+
+	if _, err = ts.redis.Set(tokenKey, encoded, t).Result(); err != nil {
+		return
+	}
+
+	return
+}
+
+// CommissionWithKey creates a single use token that expires after the given timeout. Unlike New, it accepts a
+// key rather can creating one
+func (ts *TokenStore) CommissionWithKey(t time.Duration, key string, data interface{}) (string, error) {
 	var encoded []byte
 	var err error
+	var token string
 
-	if token, err = RandomString(32); err != nil {
+	// create a hash for the key
+	if token, err = ts.getHash(key); err != nil {
 		return "", err
 	}
 
@@ -36,7 +76,7 @@ func (ts *TokenStore) New(timeout time.Duration, data interface{}) (string, erro
 	// One would naturally prefer hash maps but they don't support individual subkey expiry.
 	tokenKey := fmt.Sprintf("%s::%s", tokenMapKey, token)
 
-	if _, err = ts.redis.Set(tokenKey, encoded, timeout).Result(); err != nil {
+	if _, err = ts.redis.Set(tokenKey, encoded, t).Result(); err != nil {
 		return "", err
 	}
 
@@ -65,9 +105,9 @@ func (ts *TokenStore) Refresh(token string, timeout time.Duration, data interfac
 	return nil
 }
 
-// Destroy loads the value referenced by the token and dispenses of the token,
+// Decommission loads the value referenced by the token and dispenses of the token,
 // making it unvailable for any further use.
-func (ts *TokenStore) Destroy(token string, data interface{}) error {
+func (ts *TokenStore) Decommission(token string, data interface{}) error {
 	var err error
 
 	tokenKey := fmt.Sprintf("%s::%s", tokenMapKey, token)
@@ -82,12 +122,44 @@ func (ts *TokenStore) Destroy(token string, data interface{}) error {
 	return nil
 }
 
+// Revoke renders the token generated for the given key useless.
+func (ts *TokenStore) Revoke(key string) error {
+	var err error
+	var token string
+
+	if token, err = ts.getHash(key); err != nil {
+		return err
+	}
+
+	var del int64
+	tokenKey := fmt.Sprintf("%s::%s", tokenMapKey, token)
+	if del, err = ts.redis.Del(tokenKey).Result(); err != nil {
+		return err
+	}
+
+	// make sure it deleted a key, else no revocation happened
+	if del == 0 {
+		return ErrTokenNotFound
+	}
+
+	return nil
+}
+
+func (ts *TokenStore) getHash(key string) (string, error) {
+	sig := hmac.New(sha256.New, ts.secret)
+	if _, err := sig.Write([]byte(key)); err != nil {
+		return "", err
+	}
+
+	return hex.EncodeToString(sig.Sum(nil)), nil
+}
+
 func (ts *TokenStore) peekToken(tokenKey string, data interface{}) error {
 	var encoded string
 	var err error
 
 	if encoded, err = ts.redis.Get(tokenKey).Result(); err != nil {
-		return err
+		return ErrTokenNotFound
 	}
 
 	if err = json.Unmarshal([]byte(encoded), data); err != nil {
