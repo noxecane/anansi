8d56dc731d53c58fcbca0abc1da0008348a2975a
Arewa Olakunle <arewa.olakunle@gmail.com>
2020-07-04 18:59:42 +0100

    fix: logs and CORS should not be enabled by defailt. (#10)

--- controller.go
+++ controller.go
@@ -6,34 +6,19 @@ import (
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
-	"github.com/rs/zerolog"
 )
 
 // DefaultMiddleware sets up some middlware for a router as well as
 // liveliness(/) and not found handlers:
 // - RequestID
 // - RealIP
-// - Logger(using zerolog)
 // - Recoverer
-// - CORS
 // - RedirectSlashes
 // - Compress(with compression level of 5)
 // - Timeout(with 1 minute)
-func DefaultMiddleware(env BasicEnv, log zerolog.Logger, router *chi.Mux) {
-	if env.AppEnv == "dev" {
-		router.Use(DevCORS().Handler)
-	} else {
-		router.Use(
-			SecureCORS(
-				"https://*godview.netlify.com",
-				"https://*tsaron.com",
-				"http://localhost").
-				Handler)
-	}
-
+func DefaultMiddleware(router *chi.Mux) {
 	router.Use(middleware.RequestID)
 	router.Use(middleware.RealIP)
-	router.Use(ZeroMiddleware(log, env.AppEnv))
 	router.Use(Recoverer)
 	router.Use(middleware.RedirectSlashes)
 	router.Use(middleware.Compress(5))
@@ -51,4 +36,15 @@ func DefaultMiddleware(env BasicEnv, log zerolog.Logger, router *chi.Mux) {
 	router.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Whoops!! This route doesn't exist", 404)
 	})
+}
+
+// CORS sets CORS for the handler. It enables localhost by default when appEnv
+// is not "dev".
+func CORS(router *chi.Mux, appEnv string, origins ...string) {
+	if appEnv == "dev" {
+		router.Use(DevCORS().Handler)
+	} else {
+		origins = append(origins, "http://localhost")
+		router.Use(SecureCORS(origins...).Handler)
+	}
 }
