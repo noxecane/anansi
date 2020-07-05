9af1ad5d204e7538ccca91f7206a7fd4d3fa859a
Olakunle Arewa <arewa.olakunle@gmail.com>
2020-07-05 14:55:47 +0100

    feat: loading envs should be as easy as calling a function

--- env.go
+++ env.go
@@ -1,9 +1,31 @@
 package anansi
 
+import (
+	"errors"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/kelseyhightower/envconfig"
+)
+
 type BasicEnv struct {
 	AppEnv string `default:"dev" split_words:"true"`
 	Name   string `required:"true"`
 	Port   int    `required:"true"`
 	Scheme string `required:"true"`
 	Secret []byte `required:"true"`
+}
+
+// LoadEnv loads environment variables into Env
+func LoadEnv(env interface{}) error {
+	// try to load from .env first
+	err := godotenv.Load()
+	if err != nil {
+		perr, ok := err.(*os.PathError)
+		if !ok || !errors.Is(perr.Unwrap(), os.ErrNotExist) {
+			return err
+		}
+	}
+
+	return envconfig.Process("", env)
 }
