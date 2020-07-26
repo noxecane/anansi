ecd6a21d92970cd845c7655e979bcd3a4a31007c
Olakunle Arewa <arewa.olakunle@gmail.com>
2020-07-26 02:09:00 +0100

    ref: BasicEnv is prescriptive
    most if not all users would want to define the basic envs for their
    application

--- env.go
+++ env.go
@@ -8,14 +8,6 @@ import (
 	"github.com/kelseyhightower/envconfig"
 )
 
-type BasicEnv struct {
-	AppEnv string `default:"dev" split_words:"true"`
-	Name   string `required:"true"`
-	Port   int    `required:"true"`
-	Scheme string `required:"true"`
-	Secret []byte `required:"true"`
-}
-
 // LoadEnv loads environment variables into Env
 func LoadEnv(env interface{}) error {
 	// try to load from .env first
