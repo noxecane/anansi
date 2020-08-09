5dad936cc4fc46268e6d1d361de8a71b8d7e9e5a
Olakunle Arewa <arewa.olakunle@gmail.com>
2020-08-09 12:57:39 +0100

    ref: changed Refresh to Extend, removing the need for loading data
    Also moved tokens to it's own module.

--- tokens/token.go
+++ tokens/token.go
@@ -1,4 +1,4 @@
-package auth
+package tokens
 
 import (
 	"crypto/hmac"
@@ -15,27 +15,26 @@ var (
 	ErrTokenNotFound = errors.New("The passed token has either expired or never existed")
 )
 
-type TokenStore struct {
+type Store struct {
 	redis  *redis.Client
 	secret []byte
 }
 
-func NewTokenStore(r *redis.Client, secret []byte) *TokenStore {
-	return &TokenStore{redis: r, secret: secret}
+func NewTokenStore(r *redis.Client, secret []byte) *Store {
+	return &Store{redis: r, secret: secret}
 }
 
 // Commission creates a single use token that expires after the given timeout.
-func (ts *TokenStore) Commission(t time.Duration, key string, data interface{}) (string, error) {
+func (ts *Store) Commission(t time.Duration, key string, data interface{}) (string, error) {
 	var encoded []byte
 	var err error
 	var token string
 
-	// create a hash for the key
+	// create token from has of the key
 	sig := hmac.New(sha256.New, ts.secret)
 	if _, err := sig.Write([]byte(key)); err != nil {
 		return "", err
 	}
-
 	token = hex.EncodeToString(sig.Sum(nil))
 
 	// TODO: replace this something lighter and faster
@@ -51,46 +50,55 @@ func (ts *TokenStore) Commission(t time.Duration, key string, data interface{}) 
 }
 
 // Peek gets the data the token references without changing its lifetime.
-func (ts *TokenStore) Peek(token string, data interface{}) error {
+func (ts *Store) Peek(token string, data interface{}) error {
 	return ts.peekToken(token, data)
 }
 
-// Refresh loads the data the token references and refreshes it's lifetime so it can last
-// for as long as the given timeout. Note that it doesn't add timeout to the existing
-// lifetime but rather gives it a new one afresh.
-func (ts *TokenStore) Refresh(token string, timeout time.Duration, data interface{}) error {
+// Extend sets the new duration before an existing token times out. Note that it doesn't
+// take into account how long the old token had to expire, as it uses the new duration
+// entirely.
+func (ts *Store) Extend(token string, timeout time.Duration) error {
+	var ok bool
 	var err error
 
-	if err = ts.peekToken(token, data); err != nil {
+	if ok, err = ts.redis.Expire(token, timeout).Result(); err != nil {
 		return err
 	}
 
-	if _, err = ts.redis.Expire(token, timeout).Result(); err != nil {
-		return err
+	if !ok {
+		return ErrTokenNotFound
 	}
 
 	return nil
 }
 
 // Reset changes the contents of the token without changing it's TTL
-func (ts *TokenStore) Reset(key string, timeout time.Duration, data interface{}) error {
+func (ts *Store) Reset(key string, data interface{}) error {
 	var err error
 	var encoded []byte
 	var token string
 
+	// recreate the token from the  key.
 	sig := hmac.New(sha256.New, ts.secret)
 	if _, err := sig.Write([]byte(key)); err != nil {
 		return err
 	}
-
 	token = hex.EncodeToString(sig.Sum(nil))
 
+	// make sure the token existed before.
 	if _, err = ts.redis.Get(token).Result(); err != nil {
-		return ErrTokenNotFound
+		if err == redis.Nil {
+			return ErrTokenNotFound
+		}
+
+		return err
 	}
 
 	//	we already know the key exists
-	ttl, _ := ts.redis.TTL(token).Result()
+	ttl, err := ts.redis.TTL(token).Result()
+	if err != nil {
+		return err
+	}
 
 	// TODO: replace this something lighter and faster
 	if encoded, err = json.Marshal(data); err != nil {
@@ -105,7 +113,7 @@ func (ts *TokenStore) Reset(key string, timeout time.Duration, data interface{})
 
 // Decommission loads the value referenced by the token and dispenses of the token,
 // making it unvailable for further use.
-func (ts *TokenStore) Decommission(token string, data interface{}) error {
+func (ts *Store) Decommission(token string, data interface{}) error {
 	var err error
 
 	if err = ts.peekToken(token, data); err != nil {
@@ -120,7 +128,7 @@ func (ts *TokenStore) Decommission(token string, data interface{}) error {
 }
 
 // Revoke renders the token generated for the given key useless.
-func (ts *TokenStore) Revoke(key string) error {
+func (ts *Store) Revoke(key string) error {
 	var err error
 	var token string
 
@@ -144,12 +152,16 @@ func (ts *TokenStore) Revoke(key string) error {
 	return nil
 }
 
-func (ts *TokenStore) peekToken(tokenKey string, data interface{}) error {
+func (ts *Store) peekToken(tokenKey string, data interface{}) error {
 	var encoded string
 	var err error
 
 	if encoded, err = ts.redis.Get(tokenKey).Result(); err != nil {
-		return ErrTokenNotFound
+		if err == redis.Nil {
+			return ErrTokenNotFound
+		}
+
+		return err
 	}
 
 	if err = json.Unmarshal([]byte(encoded), data); err != nil {
