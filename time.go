0d76c9e43509fb8dd34dd68f5977d9f841fa561d
Olakunle Arewa <arewa.olakunle@gmail.com>
2020-09-13 11:10:06 +0100

    feat: added a new implementation for ReadQuery

---
+++ time.go
@@ -0,0 +1,18 @@
+package anansi
+
+import "time"
+
+var ISO_FORMATS = [3]string{"2006-01-02T15:04:05.000Z", "2006-01-02", "2006-01-02T15:04:05"}
+
+func FromISO(date string) (time.Time, error) {
+	var err error
+
+	for _, format := range ISO_FORMATS {
+		createdAt, err := time.Parse(format, date)
+		if err == nil {
+			return createdAt, nil
+		}
+	}
+
+	return time.Time{}, err
+}
