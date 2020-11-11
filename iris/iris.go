1a093cba9928a1a03b2188d4fc5ba511a71e8985
Arewa Olakunle <arewa.olakunle@gmail.com>
2020-11-11 21:54:08 +0100

    refactoring Iris API (#12)

--- iris/iris.go
+++ iris/iris.go
@@ -16,28 +16,23 @@ import (
 )
 
 type Config struct {
-	// Claims key specifies the key that JWT would attach the claims to.
-	// This makes it easy for other consumers to generate or consume the
-	// headless tokens created by iris. Defaults to "claim"
-	ClaimsKey string
 	// Secret should be a 32 byte array for generating headless tokens.
 	Secret []byte
 	// Service is the X-Origin-Service name to be appended for each request.
 	Service string
-	// Scheme is the headless scheme used by the platform for inters-service
-	// requests
-	Scheme string
+	// HeadlessScheme is the headless scheme used by the platform for inters-service requests
+	HeadlessScheme string
 	// HeadlessDuration is how long the headless tokens should last without
 	// expiring. Defaults to 1min
 	HeadlessDuration time.Duration
 }
 
-func New(conf Config) Client {
-	if conf.Service != "" {
+func NewClient(conf Config) Client {
+	if conf.Service == "" {
 		panic(errors.New("x-origins-service will be empty"))
 	}
 
-	if len(conf.Secret) == 0 || conf.Scheme == "" {
+	if len(conf.Secret) == 0 || conf.HeadlessScheme == "" {
 		panic(errors.New("will not be able to generate headless tokens"))
 	}
 
@@ -46,69 +41,61 @@ func New(conf Config) Client {
 		conf.HeadlessDuration = time.Minute
 	}
 
-	if conf.ClaimsKey == "" {
-		conf.ClaimsKey = "claim"
-	}
-
 	return Client{
-		service:          conf.Service,
-		secret:           conf.Secret,
-		scheme:           conf.Scheme,
+		serviceName:      conf.Service,
+		serviceSecret:    conf.Secret,
+		headlessScheme:   conf.HeadlessScheme,
 		headlessDuration: conf.HeadlessDuration,
 	}
 }
 
 type Client struct {
-	secret           []byte
-	service          string
-	scheme           string
+	serviceSecret    []byte
+	serviceName      string
+	headlessScheme   string
 	headlessDuration time.Duration
-	token            string
+}
+
+type Token struct {
+	value    string
+	headless bool
 }
 
 type jSendSuccess struct {
 	Data map[string]interface{} `json:"data"`
 }
 
-// Bearer creates IrisOptions that will replicate the session of the
-// source request passed to it. Do make sure the request has
-// authorization header set.
-func (c Client) Bearer(r *http.Request) (*Client, error) {
-	auth := strings.Split(r.Header.Get("Authorization"), "")
+// BearerToken sets the token to the session token store in the passed request
+func (c *Client) BearerToken(r *http.Request) (Token, error) {
+	auth := strings.Split(r.Header.Get("Authorization"), " ")
 
 	if len(auth) != 2 {
-		return nil, errors.New("Authorization header value is incorrect")
+		return Token{}, fmt.Errorf("authorization header value is incorrect: %s", r.Header.Get("Authorization"))
 	}
 
-	c.scheme = strings.TrimSpace(auth[0])
-	c.token = strings.TrimSpace(auth[1])
-
-	return &c, nil
+	return Token{strings.TrimSpace(auth[1]), false}, nil
 }
 
-// Headless creates a token to be used with the pre-existing scheme that has
-// been set. Make sure to use it off your base Client
-func (c Client) Headless(v interface{}) (*Client, error) {
-	token, err := jwt.EncodeEmbedded(c.secret, c.headlessDuration, v)
+// HeadlessToken creates a token to be used with the client's scheme
+func (c *Client) HeadlessToken(v interface{}) (Token, error) {
+	token, err := jwt.EncodeEmbedded(c.serviceSecret, c.headlessDuration, v)
 	if err != nil {
-		return nil, err
+		return Token{}, err
 	}
 
-	c.token = token
-
-	return &c, nil
+	return Token{token, true}, nil
 }
 
 // NewRequest is a wrapper around http.NNewRequest that adds the required
 // headers for distributed tracing. The requests will only last as long as the parent
 // request(it uses the request's context)
-func (c *Client) NewRequest(r *http.Request, method, url string, body io.Reader) (*http.Request, error) {
+func (c *Client) NewRequest(r *http.Request, method, url string, token Token, body io.Reader) (*http.Request, error) {
 	reqId := r.Header.Get("X-Request-Id")
 	if reqId == "" {
 		return nil, errors.New("request ID not set on base request")
 	}
 
-	if c.token == "" {
+	if token.value == "" {
 		return nil, errors.New("authentication token is not set")
 	}
 
@@ -117,22 +104,29 @@ func (c *Client) NewRequest(r *http.Request, method, url string, body io.Reader)
 		return nil, err
 	}
 
-	req.Header.Set("X-Origin-Service", c.service)
+	var scheme string
+	if token.headless {
+		scheme = c.headlessScheme
+	} else {
+		scheme = "Bearer"
+	}
+
+	req.Header.Set("X-Origin-Service", c.serviceName)
 	req.Header.Set("X-Request-Id", reqId)
-	req.Header.Set("Authorization", fmt.Sprintf("%s %s", c.scheme, c.token))
+	req.Header.Set("Authorization", fmt.Sprintf("%s %s", scheme, token.value))
 
 	return req, nil
 }
 
 // NewRequestWitchContext is the same as NewRequest, only the user can now control
 // how long before the request times out.
-func (c *Client) NewRequestWithContext(ctx context.Context, r *http.Request, method, url string, body io.Reader) (*http.Request, error) {
+func (c *Client) NewRequestWithContext(ctx context.Context, r *http.Request, method, url string, token Token, body io.Reader) (*http.Request, error) {
 	reqId := r.Header.Get("X-Request-Id")
 	if reqId == "" {
 		return nil, errors.New("request ID not set on base request")
 	}
 
-	if c.token == "" {
+	if token.value == "" {
 		return nil, errors.New("authentication token is not set")
 	}
 
@@ -141,9 +135,16 @@ func (c *Client) NewRequestWithContext(ctx context.Context, r *http.Request, met
 		return nil, err
 	}
 
-	req.Header.Set("X-Origin-Service", c.service)
+	var scheme string
+	if token.headless {
+		scheme = c.headlessScheme
+	} else {
+		scheme = "Bearer"
+	}
+
+	req.Header.Set("X-Origin-Service", c.serviceName)
 	req.Header.Set("X-Request-Id", reqId)
-	req.Header.Set("Authorization", fmt.Sprintf("%s %s", c.scheme, c.token))
+	req.Header.Set("Authorization", fmt.Sprintf("%s %s", scheme, token.value))
 
 	return req, nil
 }
--- iris/iris_test.go
+++ iris/iris_test.go
@@ -2,11 +2,14 @@ package iris
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 	"testing"
 
 	"github.com/pkg/errors"
+	"github.com/random-guys/go-siber/jwt"
 	"github.com/segmentio/encoding/json"
 	"syreclabs.com/go/faker"
 )
@@ -15,6 +18,84 @@ type jSMock struct {
 	Name    string   `json:"name"`
 	Company string   `json:"company"`
 	Emails  []string `json:"emails"`
+}
+
+func TestBearerToken(t *testing.T) {
+	tokenValue := faker.Lorem().Characters(32)
+
+	req, err := http.NewRequest("GET", "some-url", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Authorization", "Bearer "+tokenValue)
+	req.Header.Set("X-Request-ID", faker.Lorem().Characters(16))
+	fmt.Println(req.Header)
+
+	client := NewClient(Config{
+		Secret:         []byte(faker.Lorem().Characters(32)),
+		Service:        "some-service",
+		HeadlessScheme: "some-scheme",
+	})
+
+	token, err := client.BearerToken(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	irisReq, err := client.NewRequest(req, "GET", "some-other-url", token, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if irisReq.Header.Get("Authorization") != "Bearer "+tokenValue {
+		t.Errorf("Expected authorization to be \"Bearer %s\", got %s", tokenValue, irisReq.Header.Get("Authorization"))
+	}
+}
+
+func TestHeadlessToken(t *testing.T) {
+	secret := []byte(faker.Lorem().Characters(32))
+	client := NewClient(Config{
+		Secret:         secret,
+		Service:        "some-service",
+		HeadlessScheme: "some-scheme",
+	})
+
+	type sessionInfo struct {
+		Info string
+	}
+	session := sessionInfo{"some-session-info"}
+
+	token, err := client.HeadlessToken(session)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req, err := http.NewRequest("GET", "some-url", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("X-Request-ID", faker.Lorem().Characters(16))
+
+	irisReq, err := client.NewRequest(req, "GET", "some-other-url", token, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if irisReq.Header.Get("Authorization") != fmt.Sprintf("%s %s", client.headlessScheme, token.value) {
+		t.Fatalf("Expected authorization to be \"%s %s\", got %s", client.headlessScheme, token.value, irisReq.Header.Get("Authorization"))
+	}
+
+	auth := strings.Split(irisReq.Header.Get("Authorization"), " ")
+	tokenStr := strings.TrimSpace(auth[1])
+
+	var loadedSession sessionInfo
+	if err := jwt.DecodeEmbedded(secret, []byte(tokenStr), &loadedSession); err != nil {
+		t.Fatal(err)
+	}
+
+	if loadedSession.Info != session.Info {
+		t.Errorf("Expected session to store %s, got %s", session.Info, loadedSession.Info)
+	}
 }
 
 func TestGetResponse(t *testing.T) {
