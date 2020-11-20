fea9a2f1746fef45bd5f4cad39a4afc82e755a4c
Arewa Olakunle <arewa.olakunle@gmail.com>
2020-11-20 16:21:25 +0100

    feat: ajax for interservice communication (#42)

---
+++ ajax/ajax.go
@@ -0,0 +1,166 @@
+package ajax
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/pkg/errors"
+	"github.com/tsaron/anansi"
+	"github.com/tsaron/anansi/jwt"
+)
+
+type Config struct {
+	// Secret should be a 32 byte array for generating headless tokens.
+	Secret []byte
+	// Service is the X-Origin-Service name to be appended for each request.
+	Service string
+	// HeadlessScheme is the headless scheme used by the platform for inters-service requests
+	HeadlessScheme string
+	// HeadlessDuration is how long the headless tokens should last without
+	// expiring. Defaults to 1min
+	HeadlessDuration time.Duration
+}
+
+func NewClient(conf Config) Client {
+	if conf.Service == "" {
+		panic(errors.New("x-origins-service will be empty"))
+	}
+
+	if len(conf.Secret) == 0 || conf.HeadlessScheme == "" {
+		panic(errors.New("will not be able to generate headless tokens"))
+	}
+
+	// default headless sessions should last 1min
+	if conf.HeadlessDuration == 0 {
+		conf.HeadlessDuration = time.Minute
+	}
+
+	return Client{
+		serviceName:      conf.Service,
+		serviceSecret:    conf.Secret,
+		headlessScheme:   conf.HeadlessScheme,
+		headlessDuration: conf.HeadlessDuration,
+	}
+}
+
+type Client struct {
+	serviceSecret    []byte
+	serviceName      string
+	headlessScheme   string
+	headlessDuration time.Duration
+}
+
+type Token struct {
+	value    string
+	headless bool
+}
+
+// BearerToken sets the token to the session token store in the passed request
+func (c *Client) BearerToken(r *http.Request) (Token, error) {
+	auth := strings.Split(r.Header.Get("Authorization"), " ")
+
+	if len(auth) != 2 {
+		return Token{}, fmt.Errorf("authorization header value is incorrect: %s", r.Header.Get("Authorization"))
+	}
+
+	return Token{strings.TrimSpace(auth[1]), false}, nil
+}
+
+// HeadlessToken creates a token to be used with the client's scheme
+func (c *Client) HeadlessToken(v interface{}) (Token, error) {
+	token, err := jwt.EncodeEmbedded(c.serviceSecret, c.headlessDuration, v)
+	if err != nil {
+		return Token{}, err
+	}
+
+	return Token{token, true}, nil
+}
+
+// NewRequest is a wrapper around http.NNewRequest that adds the required
+// headers for distributed tracing. The requests will only last as long as the parent
+// request(it uses the request's context)
+func (c *Client) NewRequest(r *http.Request, method, url string, token Token, body io.Reader) (*http.Request, error) {
+	reqId := r.Header.Get("X-Request-Id")
+	if reqId == "" {
+		return nil, errors.New("request ID not set on base request")
+	}
+
+	if token.value == "" {
+		return nil, errors.New("authentication token is not set")
+	}
+
+	req, err := http.NewRequestWithContext(r.Context(), method, url, body)
+	if err != nil {
+		return nil, err
+	}
+
+	var scheme string
+	if token.headless {
+		scheme = c.headlessScheme
+	} else {
+		scheme = "Bearer"
+	}
+
+	req.Header.Set("X-Origin-Service", c.serviceName)
+	req.Header.Set("X-Request-Id", reqId)
+	req.Header.Set("Authorization", fmt.Sprintf("%s %s", scheme, token.value))
+
+	return req, nil
+}
+
+// NewRequestWitchContext is the same as NewRequest, only the user can now control
+// how long before the request times out.
+func (c *Client) NewRequestWithContext(ctx context.Context, r *http.Request, method, url string, token Token, body io.Reader) (*http.Request, error) {
+	reqId := r.Header.Get("X-Request-Id")
+	if reqId == "" {
+		return nil, errors.New("request ID not set on base request")
+	}
+
+	if token.value == "" {
+		return nil, errors.New("authentication token is not set")
+	}
+
+	req, err := http.NewRequestWithContext(ctx, method, url, body)
+	if err != nil {
+		return nil, err
+	}
+
+	var scheme string
+	if token.headless {
+		scheme = c.headlessScheme
+	} else {
+		scheme = "Bearer"
+	}
+
+	req.Header.Set("X-Origin-Service", c.serviceName)
+	req.Header.Set("X-Request-Id", reqId)
+	req.Header.Set("Authorization", fmt.Sprintf("%s %s", scheme, token.value))
+
+	return req, nil
+}
+
+func GetErr(res *http.Response) error {
+	if res.StatusCode < 400 {
+		return nil
+	}
+
+	var err anansi.APIError
+	if err := json.NewDecoder(res.Body).Decode(&err); err != nil {
+		return err
+	}
+	return err
+}
+
+func GetResponse(res *http.Response, v interface{}) error {
+	err := json.NewDecoder(res.Body).Decode(v)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ ajax/ajax_test.go
@@ -0,0 +1,126 @@
+package ajax
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"testing"
+
+	"github.com/pkg/errors"
+	"github.com/tsaron/anansi/jwt"
+	"syreclabs.com/go/faker"
+)
+
+type jSMock struct {
+	Name    string   `json:"name"`
+	Company string   `json:"company"`
+	Emails  []string `json:"emails"`
+}
+
+func TestBearerToken(t *testing.T) {
+	tokenValue := faker.Lorem().Characters(32)
+
+	req, err := http.NewRequest("GET", "some-url", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Authorization", "Bearer "+tokenValue)
+	req.Header.Set("X-Request-ID", faker.Lorem().Characters(16))
+	fmt.Println(req.Header)
+
+	client := NewClient(Config{
+		Secret:         []byte(faker.Lorem().Characters(32)),
+		Service:        "some-service",
+		HeadlessScheme: "some-scheme",
+	})
+
+	token, err := client.BearerToken(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ajaxReq, err := client.NewRequest(req, "GET", "some-other-url", token, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if ajaxReq.Header.Get("Authorization") != "Bearer "+tokenValue {
+		t.Errorf("Expected authorization to be \"Bearer %s\", got %s", tokenValue, ajaxReq.Header.Get("Authorization"))
+	}
+}
+
+func TestHeadlessToken(t *testing.T) {
+	secret := []byte(faker.Lorem().Characters(32))
+	client := NewClient(Config{
+		Secret:         secret,
+		Service:        "some-service",
+		HeadlessScheme: "some-scheme",
+	})
+
+	type sessionInfo struct {
+		Info string
+	}
+	session := sessionInfo{"some-session-info"}
+
+	token, err := client.HeadlessToken(session)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req, err := http.NewRequest("GET", "some-url", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("X-Request-ID", faker.Lorem().Characters(16))
+
+	ajaxReq, err := client.NewRequest(req, "GET", "some-other-url", token, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if ajaxReq.Header.Get("Authorization") != fmt.Sprintf("%s %s", client.headlessScheme, token.value) {
+		t.Fatalf("Expected authorization to be \"%s %s\", got %s", client.headlessScheme, token.value, ajaxReq.Header.Get("Authorization"))
+	}
+
+	auth := strings.Split(ajaxReq.Header.Get("Authorization"), " ")
+	tokenStr := strings.TrimSpace(auth[1])
+
+	var loadedSession sessionInfo
+	if err := jwt.DecodeEmbedded(secret, []byte(tokenStr), &loadedSession); err != nil {
+		t.Fatal(err)
+	}
+
+	if loadedSession.Info != session.Info {
+		t.Errorf("Expected session to store %s, got %s", session.Info, loadedSession.Info)
+	}
+}
+
+func TestGetResponse(t *testing.T) {
+	name := faker.Name().FirstName()
+
+	t.Run("It should decode response", func(t *testing.T) {
+		b, err := json.Marshal(map[string]interface{}{
+			"data": map[string]interface{}{"name": name},
+		})
+		if err != nil {
+			panic(err)
+		}
+
+		res := http.Response{
+			Body: ioutil.NopCloser(bytes.NewBuffer(b)),
+		}
+
+		var data jSMock
+		err = GetResponse(&res, &data)
+		if err != nil {
+			t.Fatal(errors.Wrap(err, "error getting response"))
+		}
+
+		if data.Name != name {
+			t.Fatalf("expected %s got %s", name, data.Name)
+		}
+	})
+}
