d3f2c366bd86cfb133e9d63cbf22c6d65cdd683d
Olakunle Arewa <arewa.olakunle@gmail.com>
2020-12-02 00:05:38 +0100

    feat: move requests into its own package and use general errors

---
+++ requests/body.go
@@ -0,0 +1,123 @@
+package requests
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/go-chi/chi"
+	ozzo "github.com/go-ozzo/ozzo-validation/v4"
+)
+
+var ErrNotJSON = errors.New("body is not JSON")
+
+type ErrDecode struct {
+	Err error
+}
+
+func (e ErrDecode) Error() string {
+	return "json decoding error: " + e.Error()
+}
+
+func (e ErrDecode) Unwrap() error {
+	return e.Err
+}
+
+type ErrValidation struct {
+	Err error
+}
+
+func (e ErrValidation) Error() string {
+	return "validation error: " + e.Error()
+}
+
+func (e ErrValidation) Unwrap() error {
+	return e.Err
+}
+
+// ReadBody extracts the bytes in a request body without destroying the contents of the body.
+// Returns an error if reading body fails.
+func ReadBody(r *http.Request) ([]byte, error) {
+	var buffer bytes.Buffer
+
+	// copy request body to in memory buffer while being read
+	readSplit := io.TeeReader(r.Body, &buffer)
+	body, err := ioutil.ReadAll(readSplit)
+	if err != nil {
+		return nil, err
+	}
+
+	// return what you collected
+	r.Body = ioutil.NopCloser(&buffer)
+
+	return body, nil
+}
+
+// ReadJSON decodes the JSON body of the request and destroys it to prevent possible issues with
+// writing a response. Returns ErrNotJSON if the content-type of the request is not JSON, otherwise
+// it returns either ErrDecode for JSON decoding errors or ErrValidation if the resultant value
+// fails validation defined using ozzo.
+func ReadJSON(r *http.Request, v interface{}) error {
+	// make sure we are reading a JSON type
+	contentType := r.Header.Get("Content-Type")
+	if !strings.Contains(contentType, "application/json") {
+		return ErrNotJSON
+	}
+
+	err := json.NewDecoder(r.Body).Decode(v)
+	switch {
+	case err == io.EOF:
+		// tell the user all the required attributes
+		err := ozzo.Validate(v)
+		if err != nil {
+			return ErrValidation{err}
+		}
+		return err
+	case err != nil:
+		return ErrDecode{err}
+	default:
+		// validate parsed JSON data
+		err = ozzo.Validate(v)
+		if err != nil {
+			return ErrValidation{err}
+		}
+	}
+
+	return nil
+}
+
+// IDParam extracts a uint URL parameter from the given request. panics if there's no
+// such path on the route, otherwise it returns an error if the param is not an int.
+func IDParam(r *http.Request, name string) (uint, error) {
+	param := chi.URLParam(r, name)
+	if param == "" {
+		err := fmt.Sprintf("requested param %s is not part of route", name)
+		panic(errors.New(err))
+	}
+
+	raw, err := strconv.ParseUint(param, 10, 32)
+	if err != nil {
+		err := fmt.Sprintf("%s must be an ID", name)
+		return 0, errors.New(err)
+	}
+
+	return uint(raw), nil
+}
+
+// StringParam basically just ensures the param name is correct. You might not
+// need this method unless you're too lazy to do real tests.
+func StringParam(r *http.Request, name string) string {
+	param := chi.URLParam(r, name)
+	if param == "" {
+		err := fmt.Sprintf("requested param %s is not part of route", name)
+		panic(errors.New(err))
+	}
+
+	return param
+}
