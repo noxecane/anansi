2b7c3b0667c0277f97d791e9f6be656a791c4f95
Olakunle Arewa <arewa.olakunle@gmail.com>
2020-12-06 21:39:42 +0100

    ref: split Load into 2 for better reuse and simple Load function

--- sessions.go
+++ sessions.go
@@ -12,10 +12,13 @@ import (
 )
 
 var (
-	ErrAuthorisationFormat = errors.New("Your authorization header format is invalid")
-	ErrUnsupportedScheme   = errors.New("Your scheme is not supported")
-	ErrEmptyToken          = errors.New("There was no token supplied to the authorization header")
-	ErrHeaderNotSet        = errors.New("Authorization header is not set")
+	// ErrHeaderFormat is returned when the authorization header doesn't
+	// contain 2 fields separated by whitespace.
+	ErrHeaderFormat = errors.New("incorrect authorization header format")
+	// ErrEmptyHeader is returned when the authorization header is not set
+	ErrEmptyHeader = errors.New("empty authorization header")
+	// ErrUnsupportedScheme is returned when the scheme is either not bearer nor the set custom scheme
+	ErrUnsupportedScheme = errors.New("unsupported authorization scheme")
 )
 
 type SessionStore struct {
@@ -29,47 +32,50 @@ func NewSessionStore(secret []byte, scheme string, timeout time.Duration, store 
 	return &SessionStore{store, timeout, secret, strings.ToLower(scheme)}
 }
 
-// Load retrieves a user's session object based on the session key from the Authorization
-// header or the session cookie and fails with an error if it faces any issue parsing any of them.
-func (s *SessionStore) Load(r *http.Request, session interface{}) {
-	var err error
-
-	scheme, token := getAuthorization(r)
-
-	if scheme != s.scheme && scheme != "Bearer" {
-		panic(jsend.Err{
-			Code:    http.StatusUnauthorized,
-			Message: ErrUnsupportedScheme.Error(),
-		})
-	}
-
-	if token == "" {
-		panic(jsend.Err{
-			Code:    http.StatusUnauthorized,
-			Message: ErrEmptyToken.Error(),
-		})
-	}
-
-	if scheme == "Bearer" {
-		err = s.store.Extend(r.Context(), token, s.timeout, session)
-	} else {
-		err = jwt.DecodeStruct(s.secret, []byte(token), session)
-	}
-
+// LoadBearer loads a stateful session using the session from key the authorization header.
+func (s *SessionStore) LoadBearer(r *http.Request, v interface{}) error {
+	scheme, token, err := getAuthorization(r)
 	if err != nil {
-		panic(jsend.Err{
-			Code:    http.StatusUnauthorized,
-			Message: "Your token is invalid",
-			Err:     err,
-		})
+		return err
 	}
+
+	if scheme != "bearer" {
+		return ErrUnsupportedScheme
+	}
+
+	return s.store.Extend(r.Context(), token, s.timeout, v)
+}
+
+// LoadHeadless loads a stateless session from the encoded token in the authorization header.
+func (s *SessionStore) LoadHeadless(r *http.Request, v interface{}) error {
+	scheme, token, err := getAuthorization(r)
+	if err != nil {
+		return err
+	}
+
+	if scheme != s.scheme {
+		return ErrUnsupportedScheme
+	}
+
+	return jwt.DecodeEmbedded(s.secret, []byte(token), v)
+}
+
+// Load trys both LoadBearer and LoadHeadless.
+func (s *SessionStore) Load(r *http.Request, v interface{}) error {
+	err := s.LoadBearer(r, v)
+	if err == ErrUnsupportedScheme {
+		return s.LoadHeadless(r, v)
+	}
+
+	return err
 }
 
 // Secure loads a user session into the request context
 func (s *SessionStore) Headless() func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			scheme, token := getAuthorization(r)
+			scheme, token, _ := getAuthorization(r)
+
 			if scheme != s.scheme {
 				panic(jsend.Err{
 					Code:    http.StatusUnauthorized,
@@ -98,27 +104,21 @@ func (s *SessionStore) Headless() func(http.Handler) http.Handler {
 	}
 }
 
-func getAuthorization(r *http.Request) (scheme, token string) {
+func getAuthorization(r *http.Request) (string, string, error) {
 	authHeader := r.Header.Get("Authorization")
 
 	if authHeader == "" {
 		panic(jsend.Err{
 			Code:    http.StatusUnauthorized,
-			Message: ErrHeaderNotSet.Error(),
+			Message: ErrEmptyHeader.Error(),
 		})
 	}
 
-	splitAuth := strings.Split(strings.TrimSpace(authHeader), " ")
+	splitAuth := strings.Fields(authHeader)
 
 	if len(splitAuth) != 2 {
-		panic(jsend.Err{
-			Code:    http.StatusUnauthorized,
-			Message: ErrAuthorisationFormat.Error(),
-		})
+		return "", "", ErrHeaderFormat
 	}
 
-	scheme = strings.TrimSpace(strings.ToLower(splitAuth[0]))
-	token = splitAuth[1]
-
-	return
+	return strings.ToLower(splitAuth[0]), splitAuth[1], nil
 }
