6a9028dcff4835239b537265869e52f529faf608
Olakunle Arewa <arewa.olakunle@gmail.com>
2020-12-06 20:18:35 +0100

    ref: move and refactor Recoverer middleware in jsend package

--- jsend/middleware.go
+++ jsend/middleware.go
@@ -1,32 +1,32 @@
-package middleware
+package jsend
 
 import (
 	"context"
+	"fmt"
 	"net/http"
-	"runtime/debug"
+	"os"
+	"runtime"
 
-	"github.com/random-guys/go-siber"
 	"github.com/rs/zerolog"
 )
 
+// STACK_SIZE is the number of bytes to print to stderr when recovering from a panic
+var STACK_SIZE = 12 * 1024
+
 // Recoverer creates a middleware that handles panics from chi controllers. It handles
 // printing(optionally stack trace in dev env) and responding to the client for all
-// errors except JSendErrors. Note that all errors(bar JSendError) respond with a 500
+// errors except Err.
+//
+// Note that all errors(bar Err and request context timeouts) respond with a 500
 func Recoverer(env string) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		fn := func(w http.ResponseWriter, r *http.Request) {
 			defer func() {
-				// abort early if request timed out
-				if r.Context().Err() == context.DeadlineExceeded {
-					http.Error(w, http.StatusText(http.StatusGatewayTimeout), http.StatusGatewayTimeout)
-				}
-
 				if rvr := recover(); rvr != nil && rvr != http.ErrAbortHandler {
-					if e, ok := rvr.(siber.JSendError); ok {
-						siber.SendError(r, w, e)
+					if e, ok := rvr.(Err); ok {
+						Error(r, w, e)
 					} else {
 						ctx := r.Context()
-
 						// always log errors regardless of the type
 						log := zerolog.Ctx(ctx)
 						err := rvr.(error) // it would be serious if this wasn't an error
@@ -34,10 +34,17 @@ func Recoverer(env string) func(http.Handler) http.Handler {
 
 						// give dev a chance to trace unknown errors
 						if env == "dev" || env == "test" {
-							debug.PrintStack()
+							stack := make([]byte, STACK_SIZE)
+							stack = stack[:runtime.Stack(stack, false)]
+							fmt.Fprintf(os.Stderr, "recovering from panic:\n%s", stack)
 						}
-						http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 
+						// make sure timeouts are reported as 504
+						if ctx.Err() == context.DeadlineExceeded {
+							http.Error(w, http.StatusText(http.StatusGatewayTimeout), http.StatusGatewayTimeout)
+						} else {
+							http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+						}
 					}
 				}
 			}()
