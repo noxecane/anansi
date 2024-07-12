0512a67d0e504fd3f932eac73cd570c370936215
Kunle Arewa <arewa.olakunle@gmail.com>
2024-07-12 04:34:46 +0100

    fix: path is being joined manually (#84)

--- html/files.go
+++ html/files.go
@@ -2,13 +2,17 @@ package html
 
 import (
 	"net/http"
+	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 )
 
 // Server creates route for routing requests for static files
 func Server(router *chi.Mux, folder string) {
-	fs := http.FileServer(http.Dir("./" + folder))
+	// create path from home directory to folder
+	dir := filepath.Join(".", folder)
+
+	fs := http.FileServer(http.Dir(dir))
 	handler := http.StripPrefix("/"+folder+"/", fs)
 
 	router.Get("/"+folder+"/*", handler.ServeHTTP)
